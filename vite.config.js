76fc8735a72684129eb8161a0c05e63844d155af
Kylian Bardini <kylianb1@icloud.com>
2023-07-09 18:45:54 +0200

    💚 ci: fix build + router

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSSL from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSSL()],
   server: {
-    port: 3000
+    port: 3000,
+    https: true
   },
   resolve: {
     alias: {
