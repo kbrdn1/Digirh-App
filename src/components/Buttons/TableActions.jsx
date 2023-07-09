5bf1f2a47f579662dce6fa7a9322fc2bce706a3e
SCHILTZ Lucas <lucasschiltz.etude57@gmail.com>
2023-07-09 20:00:15 +0200

    ✨ Re put table components

---
+++ src/components/Buttons/TableActions.jsx
@@ -0,0 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+export const TableActions = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    return (
+        <>
+            <FontAwesomeIcon icon={faEllipsisVertical} className="cursor-pointer" onClick={() => setIsOpen(!isOpen)} />
+            <div className={`bg-primary-5 h-40 w-36 rounded-xl ${isOpen ? '' : 'hidden'} absolute`}>
+                <a className='flex justify-center items-center gap-3 cursor-pointer text-light' href='#'>
+                    <FontAwesomeIcon icon={faEye} />
+                    <span href="#" >Voir</span>
+                </a>
+            </div>
+        </>
+    )
+}
---
+++ src/components/ProgressBars/ProgressBar.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+const ProgressBar = ({ progress }) => {
+    let progressColor = 'bg-primary'
+    if (progress > 100) {
+        progress = 100
+    }
+
+    if (progress == 100) {
+        progressColor = 'bg-green-1'
+    }
+
+    return (
+        <div>
+            <div className="bg-light relative h-4 w-full rounded-2xl">
+                <div
+                    className={`${progressColor} absolute top-0 left-0 flex h-full items-center justify-center rounded-2xl text-xs font-semibold text-white`}
+                    style={{ width: progress + '%' }}>
+                    {progress + '%'}
+                </div>
+            </div>
+        </div >
+    )
+}
+
+ProgressBar.propTypes = {
+    progress: PropTypes.number.isRequired,
+};
+
+export default ProgressBar;
