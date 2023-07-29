1b9c7225935db062bd960b69db404c0133b4a285
Kylian Bardini <kylianb1@icloud.com>
2023-07-29 16:53:20 +0200

    ✨ feat: create Toasts

--- src/components/Toasts/Success.jsx
+++ src/components/Toasts/Success.jsx
@@ -1,19 +1,29 @@
 import { faCircleCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import toastStore from '@stores/Toast'
 
-const Success = ({ text, onClose, animation }) => {
+const Success = ({ message, close, animation }) => {
+
+  const handleClose = () => {
+    document.querySelector('.toast-success').classList.remove('toast-anim')
+    document.querySelector('.toast-success').classList.add('toast-anim-out')
+    setTimeout(() => {
+      toastStore.removeToast()
+    }, 700)
+  }
+
   return (
     <div
-      className={`${
-        animation ? 'toast-anim' : null
+      className={`toast-success ${
+        animation ? 'toast-anim' : null 
       } bg-valid text-white font-semibold text-sm rounded-lg shadow-lg p-4 flex gap-2 items-center duration-200 ease-out`}
     >
       <FontAwesomeIcon icon={faCircleCheck} beat />
-      <div className="self-center">{text}</div>
-      {onClose && (
+      <div className="self-center">{message}</div>
+      {close && (
         <FontAwesomeIcon
           icon={faXmark}
-          onClick={onClose}
+          onClick={handleClose}
           className="cursor-pointer text-lg hover:text-danger duration-200 ease-out"
         />
       )}
